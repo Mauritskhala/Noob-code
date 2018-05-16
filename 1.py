f38d37418a9687b8b1bcdec5ef6a3829dc9b9dd3
宋瑞雨 <38424891+Mauritskhala@users.noreply.github.com>
2018-05-16 10:43:13 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,21 @@
+#something used to record and refer to names
 
+def init(data):
+  data['first']={}
+  data['middle']={}
+  data['last']={}
+  
+def lookup(data,label,name):
+  return data[label].get(name)
+
+def store(data,*fullnames):
+  for fullname in fullnames:
+    names = fullname.split()
+    if len(names) == 2: names.insert(1,'')
+    labels = 'first','middle','last'
+    for label,name in zip(labels,names):
+      people = lookup(data,label,name)
+      if people:
+        people.append(fullname)
+      else:
+        data[label][name]=[fullname]
